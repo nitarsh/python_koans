902cc6ad93396e5b9e23abaaffdc6d248db45948
Shrinath Balakrishnan <shrinath@theredpandas.com>
2016-04-04 22:12:23 +0530

    a

--- python2/koans/triangle.py
+++ python2/koans/triangle.py
@@ -18,8 +18,16 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    throw_exception_if_invalid_triangle(a, b, c)
+    if a == b == c: return 'equilateral'
+    if a != b != c and a != c != b: return 'scalene'
+    return 'isosceles'
+
+
+def throw_exception_if_invalid_triangle(a, b, c):
+    if a < 1 or b < 1 or c < 1: raise TriangleError, "My Message"
+    s = sorted((a, b, c))
+    if s[2] > s[0] + s[1]: raise TriangleError, "My Message"
 
 
 # Error class used in part 2.  No need to change this code.
