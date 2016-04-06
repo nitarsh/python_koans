7cb2ef312ad3e79e5d95ff4a20b1a1ef7475b372
Shrinath Balakrishnan <shrinath@theredpandas.com>
2016-04-06 19:33:46 +0530

    p

--- python2/koans/about_dice_project.py
+++ python2/koans/about_dice_project.py
@@ -15,9 +15,7 @@ class DiceSet(object):
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
-        # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = [random.randint(1, 6) for x in range(n)]
 
 
 class AboutDiceProject(Koan):
@@ -53,7 +51,7 @@ class AboutDiceProject(Koan):
         second_time = dice.values
 
         self.assertNotEqual(first_time, second_time, \
-            "Two rolls should not be equal")
+                            "Two rolls should not be equal")
 
         # THINK ABOUT IT:
         #
